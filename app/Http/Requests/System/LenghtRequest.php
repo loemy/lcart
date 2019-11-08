b1954bf8bf2e1bfdb4f6ec2eb8d54a6b31637699
loemy <loecommerce.my@gmail.com>
2019-11-08 09:31:45 +0800

    Begin length controller

--- app/Http/Requests/System/LenghtRequest.php
+++ app/Http/Requests/System/LenghtRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Customer;
+namespace App\Http\Requests\System;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CustomerRequest extends FormRequest
+class LenghtRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Models/System/Attribute/WeighthAttribute.php
+++ app/Models/System/Attribute/WeighthAttribute.php
@@ -7,7 +7,7 @@ namespace App\Models\System\Attribute;
 /**
  *
  */
-trait LengthAttribute
+trait WeightAttribute
 {
     /**
      * radio check show this this current default
@@ -30,7 +30,7 @@ trait LengthAttribute
 
     public function getEditButtonAttribute()
     {
-         return '<a href="'.route('admin.lengths.edit', $this).'" class="btn btn-circle btn-success">
+         return '<a href="'.route('admin.weight.edit', $this).'" class="btn btn-circle btn-success">
                  <i class="fa fa-pencil" data-toggle="tooltip" data-placement="top"'.__('Edit').'"></i></a>';
     }
 
@@ -41,7 +41,7 @@ trait LengthAttribute
     public function getDeleteButtonAttribute() {
 
         if(!$this->isDefault()){
-             return '<a href="" '.route('admin.lengths.destroy', $this).'"
+             return '<a href="" '.route('admin.weight.destroy', $this).'"
                    data-method = "delete"
                    data-trans-button-cancel =" '.__('cancel').'"
                    data-trans-button-confirm=" '.__('delete').'"
@@ -54,20 +54,20 @@ trait LengthAttribute
 
     public function getRestoreButtonAttribute()
     {
-        return '<a href="'.route('admin.length.restore', $this).'" name="restore_length"  class="btn btn-circle btn-primary">
+        return '<a href="'.route('admin.weight.restore', $this).'" name="restore_weight"  class="btn btn-circle btn-primary">
                 <i class="fa fa-fresh" data-toggle="tooltip" data-placement="top" title="'.__('restore').'"</i></a>  ';
     }
 
     public function getDeletePermanentlyButtonAttribute()
     {
-       return '<a href= " '.route('admin.length.delete-permanently', $this).'" name="delete_length permanently" class="btn btn-circle btn-pink">
+       return '<a href= " '.route('admin.weigth.delete-permanently', $this).'" name="delete_weigth permanently" class="btn btn-circle btn-pink">
                <i class="fa fa-trash"  data-toggle="tooltip" data-placement="top" title=" '.__('delete_permanently').'" </i></a> ';
     }
 
     public function getDefaultButtonAttribute()
     {
        if(!$this->isDefault()){
-           return '<a href="'.route('admin.length.default', $this).'" class="btn btn-circle btn-warning">
+           return '<a href="'.route('admin.weigth.default', $this).'" class="btn btn-circle btn-warning">
                    <i class="fa fa-check"  data-toggle="tooltip" data-placement="top" title=" '.__('default').'" </i></a> ';
        }
         return '';
