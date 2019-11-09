7f0acc0f2254a0ea9f8f23eea129787fc769da9a
loemy <loecommerce.my@gmail.com>
2019-11-09 19:02:00 +0800

    length repo

--- app/Repositories/Admins/System/LengthRepository.php
+++ app/Repositories/Admins/System/LengthRepository.php
@@ -4,7 +4,9 @@ namespace App\Repositories\Admin\System;
 
 use App\Models\System\Length;
 use App\Contract\Admins\System\LengthContract;
+use App\Exceptions\GeneralException;
 use Recca0120\Repository\EloquentRepository;
+use Illuminate\Support\Facades\DB;
 
 class LengthRepository extends EloquentRepository implements LengthContract
 {
@@ -20,8 +22,86 @@ class LengthRepository extends EloquentRepository implements LengthContract
 
     public function getForDataTable($status = 1, $trashed = false) {
 
-       return Customer::select(['title', 'unit', 'value', 'is_default'])
-                            
-                             ->get();
+        if($trashed == 'true') {
+            return $this->model->onlyTrashed();
+        }
+        return Length::select(['title', 'unit', 'value', 'is_default'])
+                            ->get();
+    }
+
+    /**
+     *
+     */
+
+    public function create(array $data) :Length
+    {
+       return DB::transaction(function () use ($data) {
+            $length = $this->model->create($data);
+
+            if($length){
+                event(new LengthCreate($length));
+                return $length;
+            }
+
+            throw new GeneralException(__('length::exceptions.admin.lengths.create_error'));
+       });
+    }
+
+    /**
+     * change from exist default to new drfault- -todo
+     * new data will be 1, old either to convert
+     */
+
+    public function update(Length $length, array $data) :Length
+    {
+        return DB::transaction(function () use($length, $data) {
+          if($length->update($data)){
+              $length->save();
+              event(new LengthUpdated($length));
+              return $length;
+          }
+          throw new GeneralException(__('length::exceptions.admin.length.update_error'));
+        });
+    }
+
+    public function delete(Length $length) :Length
+    {
+       if($length->deleted()){
+           event(new LengthDelete($length));
+
+           return $length;
+       }
+       throw new GeneralException(__('length::exceptions.admin.lengths.delete_error'));
+    }
+
+    public function forceDelete(Length $length) :Length
+    {
+         if(is_null($length->deleted_at))
+         {
+             throw new GeneralException(__('length::exceptions.admin.length.delete_first'));
+         }
+
+         if($length->forceDelete())
+         {
+             event(new LengthPermanentlyDeleted($length));
+             return $length;
+         }
+
+         throw new GeneralException(__('length::exceptions.admin.length.delete_error'));
+    }
+
+    public function restore(Length $length): Length
+    {
+        if(is_null($length->deleted_at))
+        {
+            throw new GeneralException(__('length::exceptions.admin.lengths.cant_restore'));
+        }
+
+        if($length->restore())
+        {
+            event(new LengthRestored($length));
+            return $length;
+        }
+        throw new GeneralException(__('length::exceptions.admin.lengths.restore_error'));
     }
 }
