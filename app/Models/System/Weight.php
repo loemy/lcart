19387093689dee95c56254b3018e021c13b4f610
loemy <loecommerce.my@gmail.com>
2019-11-11 12:55:31 +0800

    add weight model

--- app/Models/System/Weight.php
+++ app/Models/System/Weight.php
@@ -2,9 +2,51 @@
 
 namespace App\Models\System;
 
+use App\Models\System\Attribute\WeightAttribute;
+use App\Models\System\Relationship\WeightRelationship;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Weight extends Model
 {
-    //
+    use WeightAttribute,WeightRelationship,SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'weights';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'deleted_at', 'value', 'title', 'unit', 'is_default'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_default' => 'integer',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+
 }
